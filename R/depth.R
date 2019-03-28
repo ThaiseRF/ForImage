102e698622980cb614a99971a12af3301283927e
Thaise <thaisericardo.freitas@gmail.com>
2019-03-28 19:22:21 -0300

    Update depth function

--- R/depth.R
+++ R/depth.R
@@ -3,7 +3,7 @@
 #' The function retrieve the object depth information from image metadata as source.
 #'
 #'
-#' @param filePath Path to image metadata files, \code{'.xml', '.tif_meta.xml'}
+#' @param xml_file image metadata file, \code{'.xml', '.tif_meta.xml'}
 #'
 #' @return A dataframe with the folowing information:
 #' \itemize{
@@ -20,23 +20,45 @@ depth.xml <- function(xml_file) {
 
   data <- xml2::read_xml(xml_file)
 
-  # Filename
-  name <- data %>%
-    xml2::xml_find_all("//Tags/V5") %>%
-    xml2::xml_text()
-  filename <- name[1]
+  # check data
+  f <- data %>%
+      xml2::xml_find_all("//Tags/I58") %>%
+      xml2::xml_text() %>%
+      as.integer()
 
-  # First
-  one <- data %>%
-    xml2::xml_find_all("//Tags/V58") %>%
-    xml2::xml_text() %>%
-    as.integer()
+  g <- data %>%
+      xml2::xml_find_all("//Tags/I59") %>%
+      xml2::xml_text() %>%
+      as.integer()
 
-  #Second
-  two <- data %>%
-    xml2::xml_find_all("//Tags/V59") %>%
-    xml2::xml_text() %>%
-    as.integer()
+  if(f[1] == 16777608 && g[1] == 16777611) {
+      # First
+      one <- data %>%
+          xml2::xml_find_all("//Tags/V58") %>%
+          xml2::xml_text() %>%
+          as.integer()
+
+      #Second
+      two <- data %>%
+          xml2::xml_find_all("//Tags/V59") %>%
+          xml2::xml_text() %>%
+          as.integer()
+
+
+  } else {
+      # First
+      one <- data %>%
+          xml2::xml_find_all("//Tags/V59") %>%
+          xml2::xml_text() %>%
+          as.integer()
+
+      #Second
+      two <- data %>%
+          xml2::xml_find_all("//Tags/V60") %>%
+          xml2::xml_text() %>%
+          as.integer()
+  }
+
 
   z_depth <- abs(one[1] - two[1])
   result <- data.frame(filename, z_depth)
